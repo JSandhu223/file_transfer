da4ba9749a57ec3917138320daa22ab1962f4bdc
JSandhu223 <jasrajsandhu01@gmail.com>
2024-04-28 12:59:34 -0600

    created skeleton for getting ip of host on Windows and Linux platforms

--- server.py
+++ server.py
@@ -1,2 +1,30 @@
 import sys
 import socket
+import platform # for determining host OS
+
+
+def get_host_windows():
+    hostname = socket.gethostname()
+    # Usage: getaddrinfo(hostname, port)
+    addr_info = socket.getaddrinfo(hostname, None)
+    for s in addr_info:
+        print(s)
+
+
+# On WSL, the private IP address may not be returned.
+# So, we will use system calls to obtain the private IP
+def get_host_linux():
+    print("Work in progress ...")
+
+
+def main():
+    os_platform = platform.system()
+    host = ''
+    if os_platform == 'Windows':
+        host = get_host_windows()
+    else:
+        host = get_host_linux()
+
+
+if __name__ == '__main__':
+    main()
