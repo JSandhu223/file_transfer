b16b248841671d35d54745fccecd83a9c16f5cff
JSandhu223 <jasrajsandhu01@gmail.com>
2024-05-24 00:14:25 -0600

    setup the struct sockaddr_in family and port fields

--- client.c
+++ client.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdint.h>
+#include <arpa/inet.h>
 #include <netinet/in.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -7,14 +9,21 @@
 
 int main(int argc, char** argv)
 {
-    char message[] = "hello";
-    printf("%s\n", message);
+    // TODO: get hostname and port from command line
+    char hostname[] = "localhost";
+    uint16_t port = 8020; // 2 bytes (just to be safe)
 
     // Address info
     struct sockaddr_in server_addr;
 
     // Zero out the struct
     memset(&server_addr, 0, sizeof(server_addr));
+    // Set family to IPv4
+    server_addr.sin_family = AF_INET;
+    // Set port number (in network byte order / big endian)
+    server_addr.sin_port = htons(port);
+
+    // DEBUG LINES
     printf("Family: %d\n", server_addr.sin_family);
     printf("Port: %d\n", server_addr.sin_port);
 
