0e333cfa238069a23513b5d66edf5edb18b1f229
JSandhu223 <jasrajsandhu01@gmail.com>
2024-05-23 20:19:37 -0600

    added libraries for posix sockets

--- client.c
+++ client.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 
 
 int main(int argc, char** argv)
 {
-    printf("Hello\n");
+    char message[] = "hello";
+    printf("%s\n", message);
 
     return 0;
 }
