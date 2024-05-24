c281342b9b3c79ac4841eda5ae5d1018d00c3317
JSandhu223 <jasrajsandhu01@gmail.com>
2024-05-23 22:22:08 -0600

    included libraries for struct sockaddr_in and memset() function

--- client.c
+++ client.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <netinet/in.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 
@@ -7,6 +9,14 @@ int main(int argc, char** argv)
 {
     char message[] = "hello";
     printf("%s\n", message);
+
+    // Address info
+    struct sockaddr_in server_addr;
+
+    // Zero out the struct
+    memset(&server_addr, 0, sizeof(server_addr));
+    printf("Family: %d\n", server_addr.sin_family);
+    printf("Port: %d\n", server_addr.sin_port);
 
     return 0;
 }
