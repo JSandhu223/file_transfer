9d452d394a398ea9c50c8c2a9b3aef2c040c492d
JSandhu223 <jasrajsandhu01@gmail.com>
2024-05-24 19:51:51 -0600

    created listening socket

---
+++ server.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdint.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+
+int main(int argc, char** argv)
+{
+    // Create socket for listening
+    int sock = socket(PF_INET, SOCK_STREAM, 0);
+    if (sock == -1)
+    {
+        printf("Failed to create listening socket\n");
+        return 1;
+    }
+    printf("Listening socket created\n");
+
+    return 0;
+}
